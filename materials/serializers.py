0b3e35d822c4934a873974063da477e8ce5de506
KateRomanova <kate.romanova@gmail.com>
2024-08-16 22:58:56 +0300

    customed serializers bug

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,9 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework import serializers
 
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
+
+    lessons_amount_in_course = serializers.SerializerMethodField()
+
+    def get_lessons_amount_in_course(self, obj):
+        if obj.lesson_set.all().count:
+            return obj.lesson_set.all().count.lesson
+        else:
+            return 0
+
     class Meta:
         model = Course
         fields = "__all__"
