419e67eb1914df1e4de2b0e76f017cebdb99df0e
KateRomanova <kate.romanova@gmail.com>
2024-08-21 13:45:41 +0300

    test for create fixed

--- materials/tests.py
+++ materials/tests.py
@@ -26,14 +26,39 @@ class LessonTestCase(APITestCase):
         data = {
             "name": "New test lesson",
             "description": "New test lesson description",
-            "video_link": "https://www.youtube.com/watch?v=gomhMmutBd9",
+            "video_url": "https://www.youtube.com/watch?v=gomhMmutBd9",
             "course": self.course.pk
         }
         response = self.client.post(url, data)
-        # self.assertEqual(
-        #     response.status_code, status.HTTP_201_CREATED
-        # )
+        print(response.json())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(
             Lesson.objects.all().count(), 2
         )
+
+    def test_lesson_update(self):
+        url = reverse('materials:lessons_update', args=(self.lesson.pk,))
+        data = {
+            "name": "New test lesson",
+            "description": "New test lesson description",
+            "video_url": "https://www.youtube.com/watch?v=gomhMmutBd9",
+            "course": self.course.pk
+        }
+        response = self.client.patch(url, data)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get('name'), "New test lesson")
+
+    def test_lesson_delete(self):
+        url = reverse('materials:lessons_delete', args=(self.lesson.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(
+            Lesson.objects.all().count(), 0
+        )
+
+    def test_lesson_list(self):
+        url = reverse("materials:lessons_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
